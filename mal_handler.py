633cccbf36a81dca028b30e25b52fb53afa68fb8
ThinkRedstone <ittai11@gmail.com>
2016-05-20 00:28:48 +0300

    Search now returns a list of Anime objects

--- mal_handler.py
+++ mal_handler.py
@@ -3,9 +3,25 @@ import xml.etree.ElementTree as ET
 
 auth = ("thinkredstone", "***REMOVED***")
 
+
 def search(anime):
     global auth
     r = requests.get('http://myanimelist.net/api/anime/search.xml', params={'q': anime}, auth=auth)
     if r.status_code is not 200:
         raise Exception
-    return r.content
+    root = ET.fromstring(r.content)
+    ret = []
+    for child in root.findall("entry"):
+        id = int(child.find("id").text)
+        title = child.find('title').text
+        ret.append(Anime(id, title))
+    return ret
+
+
+class Anime:
+    def __init__(self, id, title):
+        self.id = id
+        self.title = title
+
+    def __repr__(self):
+        return "%s (ID: %d)" % (self.title, self.id)
